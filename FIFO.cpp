1e2ab50e93d8b7c4e5052e0df73d0642a1be24d5
Henry Lin <badcop@163.com>
2018-04-17 18:30:34 +0800

    up

--- FIFO.cpp
+++ FIFO.cpp
@@ -5,11 +5,17 @@
 #include "FIFO.h"
 
 void FIFO::monitor(Request *request) {
+    //iterate the queue to determine if the requested page number already exists in the queue
+    //迭代队列以判断队列中是否已存在请求的页面编号
     for (auto it:this->loadQueue) {
+        //if page number exists, return(unnecessary to push to the queue)
+        //若页面编号已存在，返回（无需送入队列）
         if (it == request->getPageNumber()) {
             return;
         }
     }
+    //if page number not exists, push to the end of queue
+    //若页面编号不存在，送入队列末端
     this->loadQueue.push_back(request->getPageNumber());
 }
 
